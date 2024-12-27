ff923d3ba1f367a370046b7a6f0a455e03e59945
VitorR-Soares <vitorratollasoares@gmail.com>
2024-12-27 20:07:37 -0300

    Funcionalidade de Salvar (ainda não funcional)

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -12,6 +12,7 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -22,15 +23,28 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
-    public void cadastrarProduto (ProdutosDTO produto){
+    public void cadastrarProduto (ProdutosDTO produto) throws SQLException{        
+        String sql = "insert into produtos values (?,?,?)";
+        conn = new conectaDAO().connectDB();
+        prep = conn.prepareStatement(sql);
+        prep.setString(1, produto.getNome());
+        prep.setInt(2, produto.getValor());
+        prep.setString(3, produto.getStatus());        
         
+        prep.executeUpdate();
         
-        //conn = new conectaDAO().connectDB();
-        
-        
+        try {
+            conn.close();
+            System.out.println( "Conexão com o banco de dados fechada" );
+        } catch (SQLException sqle) {
+            System.out.println( "Erro no fechamento da conexão : " + sqle.getMessage());
+        }
+                
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
+        
+        
         
         return listagem;
     }
